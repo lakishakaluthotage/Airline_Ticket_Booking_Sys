7a0dc3baaf0e793b6073f701f2269978463d57f5
Lakisha Kaluthotage <95086455+lakishakaluthotage@users.noreply.github.com>
2023-06-17 10:52:15 +0530

    Added the basic Jforms

---
+++ Airline_management.java
@@ -0,0 +1,16 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package airline;
+
+/**
+ *
+ * @author USER
+ */
+public class Airline_management {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
